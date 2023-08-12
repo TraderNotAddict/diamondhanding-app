21ed00f38afcfc123cc7037b3ca386d4aa7643ad
Z <139626295+TraderNotAddict@users.noreply.github.com>
2023-08-12 10:40:25 +0800

    shift theme file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,52 +1,52 @@
-import React, { useMemo } from 'react';
-import { Toaster } from 'react-hot-toast';
-import type { AppProps } from 'next/app';
-import dynamic from 'next/dynamic';
-import { ConnectionProvider } from '@solana/wallet-adapter-react';
+import React, { useMemo } from "react";
+import { Toaster } from "react-hot-toast";
+import type { AppProps } from "next/app";
+import dynamic from "next/dynamic";
+import { ConnectionProvider } from "@solana/wallet-adapter-react";
 import {
-  SolflareWalletAdapter,
-  LedgerWalletAdapter,
-  GlowWalletAdapter,
-  PhantomWalletAdapter,
-} from '@solana/wallet-adapter-wallets';
-import { ChakraProvider } from '@chakra-ui/react';
+	SolflareWalletAdapter,
+	LedgerWalletAdapter,
+	GlowWalletAdapter,
+	PhantomWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { ChakraProvider } from "@chakra-ui/react";
 
-import ClientWalletProvider from '@/components/contexts/ClientWalletProvider';
-import { NETWORK } from '@/utils/constants/endpoints';
-import theme from './theme';
-import '../styles/globals.scss';
-import { Fonts } from '@/components/Fonts';
+import ClientWalletProvider from "@/components/contexts/ClientWalletProvider";
+import { NETWORK } from "@/utils/constants/endpoints";
+import theme from "../styles/theme";
+import "../styles/globals.scss";
+import { Fonts } from "@/components/Fonts";
 
 const ReactUIWalletModalProviderDynamic = dynamic(
-  async () =>
-    (await import('@solana/wallet-adapter-react-ui')).WalletModalProvider,
-  { ssr: false }
+	async () =>
+		(await import("@solana/wallet-adapter-react-ui")).WalletModalProvider,
+	{ ssr: false }
 );
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-  const wallets = useMemo(
-    () => [
-      new PhantomWalletAdapter(),
-      new GlowWalletAdapter(),
-      new SolflareWalletAdapter(),
-      new LedgerWalletAdapter(),
-    ],
-    []
-  );
+	const wallets = useMemo(
+		() => [
+			new PhantomWalletAdapter(),
+			new GlowWalletAdapter(),
+			new SolflareWalletAdapter(),
+			new LedgerWalletAdapter(),
+		],
+		[]
+	);
 
-  return (
-    <ConnectionProvider endpoint={NETWORK}>
-      <ClientWalletProvider wallets={wallets}>
-        <ReactUIWalletModalProviderDynamic>
-          <ChakraProvider theme={theme}>
-            <Fonts />
-            <Toaster position="top-right" reverseOrder={false} />
-            <Component {...pageProps} />
-          </ChakraProvider>
-        </ReactUIWalletModalProviderDynamic>
-      </ClientWalletProvider>
-    </ConnectionProvider>
-  );
+	return (
+		<ConnectionProvider endpoint={NETWORK}>
+			<ClientWalletProvider wallets={wallets}>
+				<ReactUIWalletModalProviderDynamic>
+					<ChakraProvider theme={theme}>
+						<Fonts />
+						<Toaster position="top-right" reverseOrder={false} />
+						<Component {...pageProps} />
+					</ChakraProvider>
+				</ReactUIWalletModalProviderDynamic>
+			</ClientWalletProvider>
+		</ConnectionProvider>
+	);
 }
 
 export default MyApp;
