ef590495185f1cb2a62dbc31137b6e71c1c576ce
Z <tradernotaddict@gmail.com>
2023-08-21 01:15:27 +0800

    fix

--- src/pages/api/memento/[walletAddress].ts
+++ src/pages/api/memento/[walletAddress].ts
@@ -15,44 +15,46 @@ import { NftCollection } from "@/models/enums/NftCollection";
 import { getCollectionMintProgress } from "@/server/services/memento/mint/getCollectionMintProgress";
 
 export default connectSolana(
-	async (
-		req: NextApiRequestWithSolanaProgram,
-		res: NextApiResponse<{
-			success: boolean;
-			userMementos: IMemento[];
-			mintProgresses?: { [key in NftCollection]: number };
-		}>
-	) => {
-		const { walletAddress } = req.query;
-		const { solanaConnection: connection, program } = req;
+	connectDB(
+		async (
+			req: NextApiRequestWithSolanaProgram,
+			res: NextApiResponse<{
+				success: boolean;
+				userMementos: IMemento[];
+				mintProgresses?: { [key in NftCollection]: number };
+			}>
+		) => {
+			const { walletAddress } = req.query;
+			const { solanaConnection: connection, program } = req;
 
-		if (!connection) {
-			return res.status(500).json({ success: false, userMementos: [] });
+			if (!connection) {
+				return res.status(500).json({ success: false, userMementos: [] });
+			}
+
+			if (walletAddress && walletAddress.length > 0) {
+				const userMementos = await getAllMementosByWalletAddress(
+					walletAddress as string
+				);
+				const mintProgresses = {
+					[NftCollection.CCSH]: await getCollectionMintProgress(
+						NftCollection.CCSH,
+						connection
+					),
+					[NftCollection.PHBC]: await getCollectionMintProgress(
+						NftCollection.PHBC,
+						connection
+					),
+					[NftCollection.Dev1b]: 5,
+					[NftCollection.Dev1c]: 0,
+					[NftCollection.Dev1]: 0,
+					[NftCollection.Dev2]: 0,
+				};
+				return res
+					.status(200)
+					.json({ success: true, userMementos, mintProgresses });
+			}
+
+			return res.status(400).end();
 		}
-
-		if (walletAddress && walletAddress.length > 0) {
-			const userMementos = await getAllMementosByWalletAddress(
-				walletAddress as string
-			);
-			const mintProgresses = {
-				[NftCollection.CCSH]: await getCollectionMintProgress(
-					NftCollection.CCSH,
-					connection
-				),
-				[NftCollection.PHBC]: await getCollectionMintProgress(
-					NftCollection.PHBC,
-					connection
-				),
-				[NftCollection.Dev1b]: 5,
-				[NftCollection.Dev1c]: 0,
-				[NftCollection.Dev1]: 0,
-				[NftCollection.Dev2]: 0,
-			};
-			return res
-				.status(200)
-				.json({ success: true, userMementos, mintProgresses });
-		}
-
-		return res.status(400).end();
-	}
+	)
 );
