d9446291304b18132c2ba84aea04761eda279ec4
Z <tradernotaddict@gmail.com>
2023-08-22 06:20:33 +0800

    fix links

--- src/components/navbar/MobileNavbar.tsx
+++ src/components/navbar/MobileNavbar.tsx
@@ -1,28 +1,41 @@
 import {
-  Drawer,
-  DrawerBody,
-  DrawerContent,
-  DrawerProps,
-  HStack,
-  Stack,
-} from '@chakra-ui/react';
-import { DiscordIcon } from '../icons/DiscordIcon';
-import { SquareIconButton } from '../buttons/SquareIconButton';
-import { GitHubIcon } from '../icons/GitHubIcon';
-import { ConnectWalletButton } from '../buttons/ConnectWalletButton';
+	Drawer,
+	DrawerBody,
+	DrawerContent,
+	DrawerProps,
+	HStack,
+	Stack,
+} from "@chakra-ui/react";
+import { DiscordIcon } from "../icons/DiscordIcon";
+import { SquareIconButton } from "../buttons/SquareIconButton";
+import { GitHubIcon } from "../icons/GitHubIcon";
+import { ConnectWalletButton } from "../buttons/ConnectWalletButton";
+import { GitBookIcon } from "../icons/GitBookIcon";
 
-export const MobileDrawer = (props: Omit<DrawerProps, 'children'>) => (
-  <Drawer placement="top" {...props}>
-    <DrawerContent>
-      <DrawerBody mt="12" mb="2" px="4">
-        <Stack spacing="4" align="stretch">
-          <ConnectWalletButton />
-          <HStack spacing="4" justifyContent="center">
-            <SquareIconButton aria-label="Discord" icon={<DiscordIcon />} />
-            <SquareIconButton aria-label="GitHub" icon={<GitHubIcon />} />
-          </HStack>
-        </Stack>
-      </DrawerBody>
-    </DrawerContent>
-  </Drawer>
+export const MobileDrawer = (props: Omit<DrawerProps, "children">) => (
+	<Drawer placement="top" {...props}>
+		<DrawerContent>
+			<DrawerBody mt="12" mb="2" px="4">
+				<Stack spacing="4" align="stretch">
+					<ConnectWalletButton />
+					<HStack spacing="4" justifyContent="center">
+						<a
+							href="https://guide.diamondhanding.io/"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<SquareIconButton aria-label="Discord" icon={<GitBookIcon />} />
+						</a>
+						<a
+							href="https://github.com/diamondhanding"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<SquareIconButton aria-label="GitHub" icon={<GitHubIcon />} />
+						</a>
+					</HStack>
+				</Stack>
+			</DrawerBody>
+		</DrawerContent>
+	</Drawer>
 );
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,61 +1,74 @@
 import {
-  Box,
-  Container,
-  HStack,
-  useBreakpointValue,
-  useDisclosure,
-} from '@chakra-ui/react';
-import { MobileDrawer } from './MobileNavbar';
-import { ToggleButton } from './ToggleButton';
-import { SquareIconButton } from '../buttons/SquareIconButton';
-import { GitHubIcon } from '../icons/GitHubIcon';
-import { DiscordIcon } from '../icons/DiscordIcon';
-import { ConnectWalletButton } from '../buttons/ConnectWalletButton';
+	Box,
+	Container,
+	HStack,
+	useBreakpointValue,
+	useDisclosure,
+} from "@chakra-ui/react";
+import { MobileDrawer } from "./MobileNavbar";
+import { ToggleButton } from "./ToggleButton";
+import { SquareIconButton } from "../buttons/SquareIconButton";
+import { GitHubIcon } from "../icons/GitHubIcon";
+import { DiscordIcon } from "../icons/DiscordIcon";
+import { ConnectWalletButton } from "../buttons/ConnectWalletButton";
+import { GitBookIcon } from "../icons/GitBookIcon";
 
 export const Navbar = () => {
-  const isDesktop = useBreakpointValue({ base: false, lg: true });
-  const mobileNavbar = useDisclosure();
-  return (
-    <Box
-      borderBottomWidth="1px"
-      borderBottomColor="#212427"
-      backgroundColor="#131315"
-      bg="bg.surface"
-      position="sticky"
-      zIndex="tooltip"
-    >
-      <Container py="2" px="2" maxWidth="100%">
-        <HStack justify="space-between">
-          <div>💎</div>
-          {isDesktop ? (
-            <HStack spacing="2">
-              <SquareIconButton
-                aria-label="Discord"
-                icon={<DiscordIcon />}
-                variant="ghost"
-              />
-              <SquareIconButton
-                aria-label="GitHub"
-                icon={<GitHubIcon />}
-                variant="ghost"
-              />
-              <ConnectWalletButton />
-            </HStack>
-          ) : (
-            <>
-              <ToggleButton
-                onClick={mobileNavbar.onToggle}
-                isOpen={mobileNavbar.isOpen}
-                aria-label="Open Menu"
-              />
-              <MobileDrawer
-                isOpen={mobileNavbar.isOpen}
-                onClose={mobileNavbar.onClose}
-              />
-            </>
-          )}
-        </HStack>
-      </Container>
-    </Box>
-  );
+	const isDesktop = useBreakpointValue({ base: false, lg: true });
+	const mobileNavbar = useDisclosure();
+	return (
+		<Box
+			borderBottomWidth="1px"
+			borderBottomColor="#212427"
+			backgroundColor="#131315"
+			bg="bg.surface"
+			position="sticky"
+			zIndex="tooltip"
+		>
+			<Container py="2" px="2" maxWidth="100%">
+				<HStack justify="space-between">
+					<div>💎</div>
+					{isDesktop ? (
+						<HStack spacing="2">
+							<a
+								href="https://guide.diamondhanding.io/"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<SquareIconButton
+									aria-label="GitBook"
+									icon={<GitBookIcon />}
+									variant="ghost"
+								/>
+							</a>
+							<a
+								href="https://github.com/diamondhanding"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<SquareIconButton
+									aria-label="GitHub"
+									icon={<GitHubIcon />}
+									variant="ghost"
+								/>
+							</a>
+							<ConnectWalletButton />
+						</HStack>
+					) : (
+						<>
+							<ToggleButton
+								onClick={mobileNavbar.onToggle}
+								isOpen={mobileNavbar.isOpen}
+								aria-label="Open Menu"
+							/>
+							<MobileDrawer
+								isOpen={mobileNavbar.isOpen}
+								onClose={mobileNavbar.onClose}
+							/>
+						</>
+					)}
+				</HStack>
+			</Container>
+		</Box>
+	);
 };
