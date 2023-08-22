8f8471807365105e823db28d324e641bacc9372d
Z <tradernotaddict@gmail.com>
2023-08-22 09:23:32 +0800

    Update getCollectionMintProgress.ts

--- src/server/services/memento/mint/getCollectionMintProgress.ts
+++ src/server/services/memento/mint/getCollectionMintProgress.ts
@@ -13,6 +13,10 @@ export const getCollectionMintProgress = async (
 	connection: Connection,
 	umi?: Umi
 ): Promise<number> => {
+	if (process.env.CLUSTER === "devnet") {
+		return 0;
+	}
+
 	if (collection === NftCollection.PHBC && umi) {
 		const candyMachine = await fetchCandyMachine(
 			umi,
