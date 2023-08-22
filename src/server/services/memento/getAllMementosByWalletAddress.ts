284bb427bb2de5fbbcbd88b0925f6808c6e232e6
Z <tradernotaddict@gmail.com>
2023-08-23 02:55:17 +0800

    most recent on top?

--- src/server/services/memento/getAllMementosByWalletAddress.ts
+++ src/server/services/memento/getAllMementosByWalletAddress.ts
@@ -3,6 +3,8 @@ import { Memento } from "@/models/memento";
 export const getAllMementosByWalletAddress = async (walletAddres: string) => {
 	const mementos = await Memento.find({
 		ownerSolanaWalletAddress: walletAddres,
-	}).lean();
+	})
+		.lean()
+		.sort({ createdAt: -1 });
 	return mementos;
 };
